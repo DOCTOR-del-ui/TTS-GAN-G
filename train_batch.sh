f6c446b57c63901916c04261871fbf741f6f059a
darkdormammu <69586674+DOCTOR-del-ui@users.noreply.github.com>
2025-12-11 16:11:22 +0800

    Reduce max_iter from 20000 to 15000

--- train_batch.sh
+++ train_batch.sh
@@ -21,7 +21,7 @@ do
             --rank 0 \
             --dataset Cmapss \
             --bottom_width 8 \
-            --max_iter 20000 \
+            --max_iter 15000 \
             --img_size 32 \
             --gen_model my_gen \
             --dis_model my_dis \
